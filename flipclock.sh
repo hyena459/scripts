861ac1c0d2878946f8a999a8ba73e1ad983eb7b9
hy3na <hyena459@gmail.com>
2022-12-13 16:24:00 +0900

    ショートカットキーからのsh起動だとwatchがうまく動いていなかったので、ループのbg処理に変更

--- flipclock.sh
+++ flipclock.sh
@@ -1,7 +1,14 @@
 
 # flipclockのイベントキャッチがバグっているので、それ用のパッチ。
-# backgroundで10秒間隔でマウスを微妙に動かして(位置は戻して)flipclockのイベントを発動させたい
-watch "xdotool mousemove_relative 10 10 | xdotool mousemove_relative -- -10 -10" &
 
-# foregroundでflipclock start
+#!/bin/bash
+# backgroundで10秒間隔でマウスを微妙に動かして(位置は戻して)flipclockのイベントだけを発動させたい
+while :
+do
+  xdotool mousemove_relative 10 10; xdotool mousemove_relative -- -10 -10
+  sleep 10
+done &
+
 flatpak run one.alynx.FlipClock
+
+kill -9 $!
